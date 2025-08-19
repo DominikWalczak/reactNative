d5ff440b2004f75a63455815dedef6b974a06fcb
Dominik Walczak <dominik.walczak2@gmail.com>
2025-08-19 17:29:27 +0200

    Inicjalizacja projektu MOBILEAPP

---
+++ my-movie-mobile-app/app/_layout.tsx
@@ -0,0 +1,32 @@
+import { Stack, Slot } from "expo-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
+import "./globals.css";
+
+export default function RootLayout() {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return(
+    <QueryClientProvider client={queryClient}>
+      <Stack>
+        <Stack.Screen
+          name="(tabs)"
+          options={{
+            title: "(tabs)",
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="details"
+          options={{
+            title: "",
+            headerStyle: {
+              backgroundColor: "#e0ecf4",
+            },
+          }}
+        />
+      </Stack>
+    </QueryClientProvider>
+  );
+
+}
