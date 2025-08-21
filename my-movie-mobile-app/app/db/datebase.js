a6354a6b5ad54f05259856717765fe3e8e9d3b7b
Dominik Walczak <dominik.walczak2@gmail.com>
2025-08-21 07:19:39 +0200

    dodaję nowe elementy bazy danych

--- my-movie-mobile-app/app/db/datebase.js
+++ my-movie-mobile-app/app/db/datebase.js
@@ -5,13 +5,19 @@ export const db = SQLite.openDatabase("movies.db");
 
 useEffect(() =>{
     db.transaction(tx => {
-        tx.executeSql(`CREATE TABLE IF NOT EXISTS movie_liked(
+        tx.executeSql(`CREATE TABLE IF NOT EXISTS movie_watched(
+            id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            movie_id TEXT NOT NULL);`)
+    },
+    tx => {
+        tx.executeSql(`CREATE TABLE IF NOT EXISTS movie_to_watch(
             id INTEGER PRIMARY KEY AUTOINCREMENT, 
             movie_id TEXT NOT NULL);`)
     },
     tx => {
         tx.executeSql(`CREATE TABLE IF NOT EXISTS movie_opinion(
             opinion_id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            opinion_rate REAL NOT NULL CHECK(opinion_rate >= 1 AND opinion_rate <= 5),
             movie_id INTEGER NOT NULL, 
             opinion_title TEXT NOT NULL, 
             opinion_desc TEXT NOT NULL, 
