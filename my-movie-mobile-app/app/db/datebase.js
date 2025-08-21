7baf4b21e81683a2a36a4f768416ea8262573c9b
Dominik Walczak <dominik.walczak2@gmail.com>
2025-08-21 06:49:44 +0200

    popawiam bazy danych oraz eksportuję bazę danych

--- my-movie-mobile-app/app/db/datebase.js
+++ my-movie-mobile-app/app/db/datebase.js
@@ -1,9 +1,21 @@
 import * as SQLite from "expo-sqlite";
+import { useEffect } from "react";
 
-const db = SQLite.openDatabase("movies.db");
-db.transaction(tx => {
-    tx.executeSql("CREATE TABLE IF NOT EXISTS movie_liked(id INTEGER PRIMARY KEY AUTOINCREMENT, movie_id TEXT NOT NULL);")
-},
-tx => {
-    tx.executeSql("CREATE TABLE IF NOT EXISTS movie_opinion(opinion_id INTEGER PRIMARY KEY AUTOINCREMENT, movie_id INTEGER NOT NULL, opinion_title TEXT NOT NULL, opinion_desc TEXT NOT NULL, FOREIGN KEY(movie_id) REFERENCE movie_liked(id));")
+export const db = SQLite.openDatabase("movies.db");
+
+useEffect(() =>{
+    db.transaction(tx => {
+        tx.executeSql(`CREATE TABLE IF NOT EXISTS movie_liked(
+            id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            movie_id TEXT NOT NULL);`)
+    },
+    tx => {
+        tx.executeSql(`CREATE TABLE IF NOT EXISTS movie_opinion(
+            opinion_id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            movie_id INTEGER NOT NULL, 
+            opinion_title TEXT NOT NULL, 
+            opinion_desc TEXT NOT NULL, 
+            FOREIGN KEY(movie_id) REFERENCE movie_liked(id));`)
+    });
 });
+
