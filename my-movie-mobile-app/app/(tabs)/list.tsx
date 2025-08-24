1a1ef59f8be7d236ba62f1bea342e9fe913b38f1
Dominik Walczak <dominik.walczak2@gmail.com>
2025-08-24 08:32:52 +0200

    Poprawiam bazę danych

--- my-movie-mobile-app/app/(tabs)/list.tsx
+++ my-movie-mobile-app/app/(tabs)/list.tsx
@@ -1,5 +1,6 @@
 import { Text, View, StyleSheet, FlatList, Image, Pressable} from "react-native";
-import { use, useState } from "react";
+import { useState } from "react";
+import db from "../db/datebase";
 
 export default function List(){
     const [watched, setWatched] = useState(false);
@@ -20,13 +21,20 @@ export default function List(){
     return(
         <View style={styles.view}>
             <Pressable onPress={() => handleWatchedChange()}><Text>Lista: {watched ? "Obejrzane" : "Do obejrzenia"}</Text></Pressable>
-            {/* <FlatList 
-                data={}
+            <FlatList 
+                data={db}
                 keyExtractor={(item => item.id)}
                 renderItem={({ item }) => (
-                    
+                    <Pressable style={styles.movieItem}>
+                    <Image style={styles.movieItemImage} source={{uri: item.Poster}}/>
+                    <View style={styles.movieItemView}>
+                        <Text style={styles.movieItemViewTitle}>Title: {item.Title}</Text>
+                        <Text style={styles.movieItemViewYear}>Year: {item.Year}</Text>
+                        <Text style={styles.movieItemViewYear}>imdbID: {item.imdbID}</Text>
+                    </View>
+                    </Pressable>
                 )}
-            /> */}
+            />
         </View>
     )
 }
@@ -35,5 +43,39 @@ const styles = StyleSheet.create({
         flex: 1,
         marginTop: 35,
         backgroundColor: "#8856a7",
+  },
+    movieItem: {
+        display: "flex",
+        flexDirection: "row",
+        height: 200,
+        marginTop: 10,
+        marginLeft: 10,
+        marginRight: 10,
+        borderRadius: 30,
+        overflow: "hidden",
+        backgroundColor: "#e0ecf4",   
+        shadowColor: '#000',      
+        shadowOffset: { width: 0, height: 2 }, 
+        shadowOpacity: 1,          
+        shadowRadius: 4,           
+        elevation: 5,         
+  },
+    movieItemImage: {
+        height: 200,
+        width: 120,
+  },
+    movie: {
+        paddingBottom: 20,
+  },
+    movieItemView: {
+        padding: 10,
+  },
+    movieItemViewTitle: {
+        width: 200,
+        fontSize: 22,
+        marginBottom: 10,
+  },
+    movieItemViewYear: {
+
   },
 })
--- my-movie-mobile-app/app/db/datebase.ts
+++ my-movie-mobile-app/app/db/datebase.ts
@@ -1,27 +1,28 @@
 import * as SQLite from "expo-sqlite";
 import { useEffect } from "react";
 
-const db = (SQLite as any).openDatabase("movies.db");
+export const db = (SQLite as any).openDatabaseSync("movies.db");
 
-useEffect(() => {
-  db.transaction((tx: any) => {
-    tx.executeSql(`CREATE TABLE IF NOT EXISTS movie_watched (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      movie_id TEXT NOT NULL
-    );`);
+export default function DateBase(){
+  useEffect(() => {
+     const dbSet = async () => await db.withTransactionAsync(async (tx) => {
+      await tx.executeSqlAsync(
+        `CREATE TABLE IF NOT EXISTS movie_list (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        watched BOOLEAN
+        movie_id TEXT NOT NULL
+      );`);
 
-    tx.executeSql(`CREATE TABLE IF NOT EXISTS movie_to_watch (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      movie_id TEXT NOT NULL
-    );`);
+      await tx.executeSqlAsync(`CREATE TABLE IF NOT EXISTS movie_opinion (
+        opinion_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        opinion_rate REAL NOT NULL CHECK(opinion_rate >= 1 AND opinion_rate <= 5),
+        movie_id INTEGER NOT NULL,
+        opinion_title TEXT NOT NULL,
+        opinion_desc TEXT NOT NULL,
+        FOREIGN KEY(movie_id) REFERENCES movie_list(id)
+      );`);
+    });
+  }, []);
 
-    tx.executeSql(`CREATE TABLE IF NOT EXISTS movie_opinion (
-      opinion_id INTEGER PRIMARY KEY AUTOINCREMENT,
-      opinion_rate REAL NOT NULL CHECK(opinion_rate >= 1 AND opinion_rate <= 5),
-      movie_id INTEGER NOT NULL,
-      opinion_title TEXT NOT NULL,
-      opinion_desc TEXT NOT NULL,
-      FOREIGN KEY(movie_id) REFERENCES movie_watched(id)
-    );`);
-  });
-}, []);
+
+}
